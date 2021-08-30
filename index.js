7535d260effb886f6ac9f616d0a840114c4de050
Samuel <samlee1097@yahoo.com>
2021-08-30 11:26:59 -0400

    commit tree 67d7d2ef01d6b5028350837fbc4922c0deb68f80

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2,
+      num2 = 31,
+      multiply = num1 * num2,
+      random = Math.ceil(Math.random() * 10**(Math.ceil(Math.random() * 10))),
+      num3 = 10,
+      num4 = 6,
+      mod = num3 % num4,
+      max = Math.max(1,3,4,20,12,11);
